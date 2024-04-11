51eed0b571d9fd9548000be3a2dd5565687275ec
Youssef Roshdy <elroshdy23@gmail.com>
2024-04-11 15:54:30 +0200

    Priority Queue imp

--- OS-Scheduler/OS-Scheduler/headers.h
+++ OS-Scheduler/OS-Scheduler/headers.h
@@ -1,4 +1,4 @@
-#include <stdio.h>      //if you don't use scanf/printf change this include
+#include <stdio.h> //if you don't use scanf/printf change this include
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/file.h>
@@ -17,37 +17,32 @@ typedef short bool;
 
 #define SHKEY 300
 
-
 ///==============================
-//don't mess with this variable//
-int * shmaddr;                 //
+// don't mess with this variable//
+int *shmaddr; //
 //===============================
-
-
 
 int getClk()
 {
     return *shmaddr;
 }
 
-
 /*
  * All process call this function at the beginning to establish communication between them and the clock module.
  * Again, remember that the clock is only emulation!
-*/
+ */
 void initClk()
 {
     int shmid = shmget(SHKEY, 4, 0444);
     while ((int)shmid == -1)
     {
-        //Make sure that the clock exists
+        // Make sure that the clock exists
         printf("Wait! The clock not initialized yet!\n");
         sleep(1);
         shmid = shmget(SHKEY, 4, 0444);
     }
-    shmaddr = (int *) shmat(shmid, (void *)0, 0);
+    shmaddr = (int *)shmat(shmid, (void *)0, 0);
 }
-
 
 /*
  * All process call this function at the end to release the communication
@@ -55,7 +50,7 @@ void initClk()
  * Again, Remember that the clock is only emulation!
  * Input: terminateAll: a flag to indicate whether that this is the end of simulation.
  *                      It terminates the whole system and releases resources.
-*/
+ */
 
 void destroyClk(bool terminateAll)
 {
@@ -66,11 +61,191 @@ void destroyClk(bool terminateAll)
     }
 }
 
-
-struct process
+enum ProcessState
 {
-int id ;
-int arrival_time;
-int runtime;
-int priority;
+    ready,
+    running,
+    blocked,
+    terminated
 };
+
+struct Process
+{
+    int id;
+    int pid;
+    int arrival_time;
+    int runtime;
+    int priority;
+    int waitingTime;
+    int remainingTime;
+    int lastStopTime;
+    enum ProcessState state;
+};
+
+struct ProcessNode
+{
+    struct Process *process;
+    struct ProcessNode *next;
+};
+
+//-------------------------- RR headers ------------------------//
+
+struct CircularQueue
+{
+    struct ProcessNode *front;
+    struct ProcessNode *rear;
+};
+
+void initializeCircularQueue(struct CircularQueue *q)
+{
+    q->front = NULL;
+    q->rear = NULL;
+}
+
+void enqueueInCQ(struct CircularQueue *q, struct Process *p)
+{
+    struct ProcessNode *newProcess;
+    newProcess = (struct ProcessNode *)malloc(sizeof(struct ProcessNode));
+    newProcess->process = p;
+    newProcess->next = NULL;
+
+    if (q->front == NULL && q->rear == NULL)
+    { // if empty queue
+        q->front = newProcess;
+        (q->front)->next = q->front;
+        q->rear = q->front;
+        // printf("process enqueued");
+    }
+    else
+    {
+        q->rear->next = newProcess;
+        q->rear = newProcess;
+        (q->rear)->next = q->front;
+        // printf("process enqueued");
+    }
+}
+
+struct Process *dequeueOFCQ(struct CircularQueue *q)
+{
+    if (q->front != NULL)
+    {
+        struct ProcessNode *n = q->front;
+        struct Process *p = (q->front)->process;
+
+        if (q->front == q->rear)
+        {
+            q->front = NULL;
+            q->rear = NULL;
+        }
+        else
+        {
+            (q->rear)->next = (q->front)->next;
+            q->front = (q->front)->next;
+            n->next = NULL;
+        }
+        free(n);
+        return p;
+    }
+    else
+        return NULL;
+}
+
+void displayCQ(struct CircularQueue q)
+{
+    struct ProcessNode *it = q.front;
+    while (it != NULL && it != q.rear)
+    {
+        printf("processID:%d\n", (it->process)->id);
+        it = it->next;
+    }
+    if (it != NULL)
+        printf("processID:%d\n", (it->process)->id);
+    return;
+}
+
+bool isEmpty(struct CircularQueue q)
+{
+    if (q.front == NULL)
+        return true;
+    else
+        return false;
+}
+
+//------------------------------------------------------------
+
+// -----------------------------------------Priority Queue Data Structure ------------------------------
+
+struct priority_Queue {
+struct ProcessNode* front;
+};
+
+void initializePriorityQueue(struct priority_Queue *q)
+{
+    q->front = NULL;
+    
+}
+// enqueue dequeue peek isempty display 
+
+void priority_enqueue(struct priority_Queue *q, struct Process *p)
+{
+   struct ProcessNode *newProcess;
+    newProcess = (struct ProcessNode *)malloc(sizeof(struct ProcessNode));
+    newProcess->process = p;
+    newProcess->next = NULL;
+
+    if(q->front==NULL||q->front->process->priority>newProcess->process->priority)
+    {
+        newProcess->next=q->front;
+        q->front=newProcess;
+    }
+    else
+    {
+        struct ProcessNode* temp=q->front;
+
+        while(temp->next&&temp->next->process->priority<=p->priority)temp=temp->next;
+        newProcess->next=temp->next;
+        temp->next=newProcess;
+    }
+}
+
+struct Process* priority_peek(struct priority_Queue *q)
+{
+    if(q->front)return q->front->process;
+    else return NULL;
+}
+
+struct Process* priority_dequeue(struct priority_Queue *q)
+{
+    if(q->front)
+    {
+        struct  Process* temp= q->front->process;
+        struct ProcessNode* temp2=q->front;
+        q->front=q->front->next;
+        free(temp2);
+        return temp;
+    }
+    return NULL;
+
+}
+
+
+bool priority_isempty(struct priority_Queue *q)
+{
+    return (q->front==NULL);
+}
+
+void priority_display(struct priority_Queue *q)
+{
+    struct ProcessNode* it=q->front;
+
+  
+
+    while(it)
+    {
+        printf("process id = %d , arrival time = %d , priority = %d \n",it->process->priority, it->process->id,it->process->arrival_time);
+        it=it->next;
+        
+    } 
+}
+
+//------------------------------------------------------------------------
--- OS-Scheduler/OS-Scheduler/scheduler.c
+++ OS-Scheduler/OS-Scheduler/scheduler.c
@@ -1,21 +1,204 @@
 #include "headers.h"
 
-int msgqid,sigshmid;
-int* sigshmaddr;
-int main(int argc, char * argv[])
+
+void RR(int quantum);
+void HPF();
+int quantum;
+
+int numofProcesses;             // total number of processes comming from generator
+int arrivedProcessesNum = 0;    // number of arrived processes
+int terminatedProcessesNum = 0; // total number of terminated processes
+int algorithm = -1;
+struct Process *RunningProcess;
+struct Process *processRecv;
+
+int msgqid, sigshmid;
+int *sigshmaddr;
+
+int runPshmid; // shared memory for the remain time of the running process
+int *runPshmadd;
+//-------------------------------main function---------------------------//
+int main(int argc, char *argv[])
 {
     initClk();
-    
-                        //TODO implement the scheduler :)
-                       //upon termination release the clock resources.
+    // printf("hi from scheduler\n");
 
+    key_t key = ftok("key", 'p');
+    msgqid = msgget(key, 0666 | IPC_CREAT); // this message queue will comunicate process generator with scheduler
 
+    if (msgqid == -1)
+    {
+        perror("Error in creating message queue");
+        exit(-1);
+    }
 
-                         key_t key = ftok("key", 'p');
-        msgqid = msgget(key, 0666 | IPC_CREAT);// this message queue will comunicate process generator with scheduler
+    algorithm = atoi(argv[1]); // initializing variables
+    quantum = atoi(argv[2]);
+    numofProcesses = atoi(argv[3]);
 
-        printf("hi from scheduler\n");
+    // printf("%d ,%d ,%d ", algorithm, quantum, numofProcesses);
+    //    TODO implement the scheduler :)
 
+    key_t shmkey = ftok("key", 'r'); // shared memory for the remain time of the running process
+    runPshmid = shmget(shmkey, 4, 0666 | IPC_CREAT);
+
+    runPshmadd = (int *)shmat(runPshmid, (void *)0, 0);
+
+    switch (algorithm)
+    {
+    case 1:
+        HPF();
+        break;
+    case 2:
+        RR(quantum);
+        break;
+
+    default:
+        break;
+    }
+
+    // upon termination release the clock resources.
 
     // destroyClk(true);
+}
+
+//-----------------------------processes operation functions--------------------//
+int forkProcess(struct Process *p)
+{
+    p->pid = fork();
+    p->waitingTime = 0;
+    if (p->pid != 0)
+    {
+        printf("the scheduler forked a process with pid=%d\n", p->pid);
+        (*runPshmadd) = p->remainingTime;
+    }
+    if (p->pid == 0)
+    {
+
+        execl("./process.out", "process", NULL);
+    }
+
+    return p->pid;
+}
+
+void stopProcess(struct Process *p)
+{
+    p->lastStopTime = getClk();
+    printf("the scheduler stoped process with pid=%d\n", p->pid);
+    kill(p->pid, SIGSTOP);
+    p->remainingTime = (*runPshmadd);
+}
+
+void contProcess(struct Process *p)
+{
+    p->waitingTime += getClk() - p->lastStopTime;
+    printf("the scheduler sent continue signal to process with pid=%d\n", p->pid);
+    (*runPshmadd) = p->remainingTime;
+    kill(p->pid, SIGCONT);
+}
+
+
+//------------------------RR algorithm-----------------------------------//
+void RR(int quantum)
+{
+    printf("executing RR algorithm\n");
+    struct CircularQueue readyQueue; // queue to store arrived processes
+    initializeCircularQueue(&readyQueue);
+    int remainQuantum = quantum;
+    int currentTime = getClk();
+
+    //---creating processes (should be replaced by msgrcv)-----
+    processRecv = (struct Process *)malloc(sizeof(struct Process));
+    processRecv->remainingTime = 5;
+    processRecv->pid = forkProcess(processRecv);
+    stopProcess(processRecv);
+    enqueueInCQ(&readyQueue, processRecv);
+    processRecv = (struct Process *)malloc(sizeof(struct Process));
+    processRecv->remainingTime = 4;
+    processRecv->pid = forkProcess(processRecv);
+    stopProcess(processRecv);
+    enqueueInCQ(&readyQueue, processRecv);
+    RunningProcess = NULL;
+    numofProcesses = 2;
+    //-------------------till here-----------------------
+
+    while (getClk() == 0)
+        ;
+    while (1)
+    {
+        if (terminatedProcessesNum == numofProcesses) // if all processes are terminated
+            break;
+
+        if (RunningProcess == NULL)
+        { // there is No valid running process
+            if (!isEmpty(readyQueue))
+            {
+                remainQuantum = quantum;
+                RunningProcess = dequeueOFCQ(&readyQueue);
+                //(*runPshmadd) = RunningProcess->remainingTime;
+                contProcess(RunningProcess);
+                RunningProcess->state = running;
+            }
+        }
+        else if (RunningProcess->remainingTime == 0 || remainQuantum == 0)
+        {
+            if (RunningProcess->remainingTime == 0) // terminate the process
+            {
+                printf("process with pid=%d terminated\n", RunningProcess->pid);
+                printf("waiting time=%d\n",RunningProcess->waitingTime);
+                RunningProcess->state = terminated;
+                terminatedProcessesNum++;
+                RunningProcess = NULL;
+            }
+            else
+            {                                  // switch processes
+                RunningProcess->state = ready; // change state to ready
+                enqueueInCQ(&readyQueue, RunningProcess);
+                stopProcess(RunningProcess); // send stop signal to this process
+                RunningProcess = NULL;
+            }
+            if (!isEmpty(readyQueue))
+            {
+                printf("Context switching\n");
+                remainQuantum = quantum;
+                RunningProcess = dequeueOFCQ(&readyQueue);
+                RunningProcess->state = running;
+                //(*runPshmadd) = RunningProcess->remainingTime;
+                contProcess(RunningProcess); // send cont signal to this process
+            }
+            else
+                RunningProcess = NULL;
+        }
+        if (RunningProcess != NULL && RunningProcess->remainingTime > 0 && remainQuantum > 0)
+        {
+            RunningProcess->remainingTime = (*runPshmadd);
+            remainQuantum -= 1;
+        }
+        // if (arrivedProcessesNum < numofProcesses)
+        // {
+        //     printf("recv block\n");                              //check if there is any recieved processes
+        //     int val = msgrcv(msgqid, &processRecv, sizeof(struct Process),0,!IPC_NOWAIT);
+        //     if (val == -1)
+        //     {
+        //         perror("Error in Receiving");
+        //         break;
+        //     }
+        //     else{
+        //         printf("RR: i received a process %d\n",processRecv.id);
+        //         forkProcess(&processRecv);
+        //         stopProcess(&processRecv);
+        //         enqueueInCQ(&readyQueue,&processRecv);
+        //     }
+        // }
+        while (currentTime == getClk())
+            ;                   // waiting till the next clk
+        currentTime = getClk(); // update time
+        printf("%d\n", currentTime);
+    }
+}
+
+//---------------------HPF---------------------------------------------
+void HPF()
+{
+
 }
